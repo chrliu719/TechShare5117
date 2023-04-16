ac8cd68206ad4b3c7c658a69035affd093f97b4d
Michael Ciao <michaelciao@Michaels-MacBook-Pro-44.local>
2023-04-16 16:16:01 -0500

    layout

---
+++ techshare/src/pages/dragdrop.js
@@ -0,0 +1,30 @@
+export default function Home() {
+    return<>
+        <div className="title">
+        <h1>Move the Images</h1>
+        </div>
+        <div className="columns">
+            <div className="col outer title">col 1 BAD</div>
+            <div className="col middle title">
+                col 2 images
+                <div className="imgcols">
+                    <div className="imgstack">
+                        <img className="dragImg" src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/zdstmm4sloqnichybgja"></img>
+                        <img className="dragImg" src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/zdstmm4sloqnichybgja"></img>
+                        <img className="dragImg" src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/zdstmm4sloqnichybgja"></img>
+                        <img className="dragImg" src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/zdstmm4sloqnichybgja"></img>
+                        <img className="dragImg" src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/zdstmm4sloqnichybgja"></img>
+                    </div>
+                    <div className="imgstack">
+                        <img className="dragImg" src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/zdstmm4sloqnichybgja"></img>
+                        <img className="dragImg" src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/zdstmm4sloqnichybgja"></img>
+                        <img className="dragImg" src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/zdstmm4sloqnichybgja"></img>
+                        <img className="dragImg" src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/zdstmm4sloqnichybgja"></img>
+                        <img className="dragImg" src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/zdstmm4sloqnichybgja"></img>
+                    </div>
+                </div>
+            </div>
+            <div className="col outer title">col 3 GOod</div>
+        </div>
+    </>
+}
