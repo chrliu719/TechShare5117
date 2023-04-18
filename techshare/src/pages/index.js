0f3abf1b5b31e029fffe9007a5216c25068c69f7
Jonathan Leibovich <60627566+leamur88@users.noreply.github.com>
2023-04-18 14:21:05 -0500

    pulling images now from upload

--- techshare/src/pages/index.js
+++ techshare/src/pages/index.js
@@ -5,14 +5,20 @@ import { useRouter } from 'next/router'
 
 export default function Home() {
 	const router = useRouter()
-
+	// ["https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4383351.png&w=350&h=254", "https://static.www.nfl.com/image/private/t_headshot_desktop/league/h9ndf9ralxifgjvot2q4", "https://b.fssta.com/uploads/application/nfl/headshots/13985.png",
+	// 			"https://static.www.nfl.com/image/private/t_headshot_desktop/league/vs40h82nvqaqvyephwwu", "https://static.www.nfl.com/image/private/t_headshot_desktop/league/hdwbdlyiose4znenx5ed" ]
 	function handleClick () {
+		var imgs = []
+		var num = parseInt(document.getElementById('numCol').value)
+		const files = document.getElementById('images').files
+		for (var i = 0; i < files.length; i++){
+			imgs.push(URL.createObjectURL(files[i]))
+		}
 		router.push({
 			pathname: "/dragdrop",
 			query: {
-				imgs: ["https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4383351.png&w=350&h=254", "https://static.www.nfl.com/image/private/t_headshot_desktop/league/h9ndf9ralxifgjvot2q4", "https://b.fssta.com/uploads/application/nfl/headshots/13985.png",
-				"https://static.www.nfl.com/image/private/t_headshot_desktop/league/vs40h82nvqaqvyephwwu", "https://static.www.nfl.com/image/private/t_headshot_desktop/league/hdwbdlyiose4znenx5ed" ],
-				num: 3
+				imgs: imgs,
+				num: num
 			}
 		})
 	}
@@ -22,6 +28,9 @@ export default function Home() {
         <h1>Move the Images</h1>
 	</div>
 	<div className='UploadImg'>
+		<input type="file" id="images" name="images" accept='image/*' multiple/>
+		<label for="numCol">Number of columns: </label>
+		<input type="text" id="numCol" name="numCol" defaultValue={0}/>
 		<button type='button' onClick={handleClick} text>Create Tiered Page!</button>
 	</div>
 
