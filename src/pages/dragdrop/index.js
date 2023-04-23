c1d7af914b2fab81aad78d7949a6d3f11f37d113
Jonathan Leibovich <60627566+leamur88@users.noreply.github.com>
2023-04-23 14:58:38 -0500

    comments

--- src/pages/dragdrop/index.js
+++ src/pages/dragdrop/index.js
@@ -64,75 +64,74 @@ export default function Home() {
             setCols(tempCols);
             setColSets(tempSets);
 
-            // interact('.dragImg')
-            // .draggable({
-            //     modifiers: [
-            //         // restrict elements to only stay within elements with the class columns
-            //         // endOnly means this restriction is enacted when the event (ie. dragging) is done
-            //         interact.modifiers.restrictRect({
-            //             restriction: '.columns',
-            //             endOnly: true
-            //         })
-            //     ],
-            //     autoScroll: true,
-            //     // listener that is called when the element is dragged
-            //     listeners: {
-            //         move: function dragMoveListener (event) {
-            //             // target is the element being interacted with
-            //             var target = event.target
-            //             // Get the current data-x and data-y
-            //             // First time interacting with an object these won't exist so we start with 0
-            //             // Add the change in x and y that this dragging event caused
-            //             var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-            //             var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+            interact('.dragImg')
+            .draggable({
+				// listeners: {
+                //     move: function dragMoveListener (event) {
+                //         // target is the element being interacted with
+                //         var target = event.target
+                //         // Get the current data-x and data-y
+                //         // First time interacting with an object these won't exist so we start with 0
+                //         // Add the change in x and y that this dragging event caused
+                //         var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+                //         var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
             
-            //             // translate the element
-            //             target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+                //         // translate the element
+                //         target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
                     
-            //             // set data-x and data-y for future use
-            //             target.setAttribute('data-x', x)
-            //             target.setAttribute('data-y', y)
-            //         } 
-            // }
-            // })
+                //         // set data-x and data-y for future use
+                //         target.setAttribute('data-x', x)
+                //         target.setAttribute('data-y', y)
+                //     } 
+            	// },
+                // modifiers: [
+                //     // restrict elements to only stay within elements with the class columns
+                //     // endOnly means this restriction is enacted when the event (ie. dragging) is done
+                //     interact.modifiers.restrictRect({
+                //         restriction: '.columns',
+                //         endOnly: true
+                //     })
+                // ],
+                // autoScroll: true                
+            })
             // .resizable({
-            //     // resize from all edges and corners
-            //     edges: { left: true, right: true, bottom: true, top: true },
-            
-            //     listeners: {
-            //         move (event) {
-            //             // target is the element being interacted with
-            //             var target = event.target
-            //             // Get the current data-x and data-y
-            //             // First time interacting with an object these won't exist so we start with 0
-            //             var x = (parseFloat(target.getAttribute('data-x')) || 0)
-            //             var y = (parseFloat(target.getAttribute('data-y')) || 0)
+			// 	// edges: { left: true, right: true, bottom: true, top: true },
+            //     // listeners: {
+            //     //     move (event) {
+            //     //         // target is the element being interacted with
+            //     //         var target = event.target
+            //     //         // Get the current data-x and data-y
+            //     //         // First time interacting with an object these won't exist so we start with 0
+            //     //         var x = (parseFloat(target.getAttribute('data-x')) || 0)
+            //     //         var y = (parseFloat(target.getAttribute('data-y')) || 0)
                 
-            //             // update the width and height of the element
-            //             // event.rect is an object with information about the new dimensions
-            //             target.style.width = event.rect.width + 'px'
-            //             target.style.height = event.rect.height + 'px'
+            //     //         // update the width and height of the element
+            //     //         // event.rect is an object with information about the new dimensions
+            //     //         target.style.width = event.rect.width + 'px'
+            //     //         target.style.height = event.rect.height + 'px'
                 
-            //             // translate when resizing from top or left edges
-            //             // if resizing from the top or left, we need to change where the top left corner of the element is
-            //             // as well as resize it
-            //             x += event.deltaRect.left
-            //             y += event.deltaRect.top
+            //     //         // // translate when resizing from top or left edges
+            //     //         // // if resizing from the top or left, we need to change where the top left corner of the element is
+            //     //         // // as well as resize it
+            //     //         x += event.deltaRect.left
+            //     //         y += event.deltaRect.top
                 
-            //             // set the transform with the calculated values
-            //             target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
+            //     //         // set the transform with the calculated values
+            //     //         target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
                 
-            //             // set data-x and data-y for future use
-            //             target.setAttribute('data-x', x)
-            //             target.setAttribute('data-y', y)
-            //         }
-            //     },
-            //     modifiers: [      
-            //         // minimum size
-            //         interact.modifiers.restrictSize({
-            //             min: { width: 70, height: 70 }
-            //         })
-            //     ],
+            //     //         // set data-x and data-y for future use
+            //     //         target.setAttribute('data-x', x)
+            //     //         target.setAttribute('data-y', y)
+            //     //     }
+            //     // },
+				
+            //     // modifiers: [      
+            //     //     // maximum size
+            //     //     interact.modifiers.restrictSize({
+            //     //         max: { width: 300, height: 300 },
+			// 	// 		min: { width: 70, height: 70 }
+            //     //     })
+            //     // ],
             // })
         }
     }, [router.isReady]);
